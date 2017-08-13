b5ff6e0ab733dcf29327345f2400c8727fa14df4
Minsun Lee <dalzony@gmail.com>
2017-08-13 10:11:25 +0900

    시계영역 font 수정

--- dist/scripts.js
+++ dist/scripts.js
@@ -882,10 +882,10 @@ $(document).ready(function() {
 		// + minutes + "Minutes " + seconds + "Seconds ";
 
 		// Display the result in an element with id="demo"
-		document.getElementById("days").innerHTML = days +" <small>days</small>";
-		document.getElementById("hours").innerHTML = hours + " <small>hours</small> ";
-		document.getElementById("minutes").innerHTML = minutes + " <small>minutes</small> ";
-		document.getElementById("seconds").innerHTML = seconds + " <small>seconds</small> ";
+		document.getElementById("days").innerHTML = days +" <small>일</small>";
+		document.getElementById("hours").innerHTML = hours + " <small>시간</small> ";
+		document.getElementById("minutes").innerHTML = minutes + " <small>분</small> ";
+		document.getElementById("seconds").innerHTML = seconds + " <small>초</small> ";
 
 		// If the count down is finished, write some text
 		if (distance < 0) {
